8ecb0780aa073e9c08f1fab4e57752706f28de3f
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-03-23 17:32:45 -0400

    Add plugins for specific filetypes.

---
+++ lua/plugins/filetypes.lua
@@ -0,0 +1,14 @@
+return {
+	-- Markdown.
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = "markdown",
+		opts = {},
+	},
+	-- Fountain (Screenwriting).
+	{
+		"0mykull/nvim-fountain",
+		ft = "fountain",
+		opts = {},
+	},
+}
